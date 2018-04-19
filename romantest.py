0fb3b3df70b3d5ea4c842c1ef80f122d3f2329cd
Unknown <wadh1n1@hotmail.com>
2018-04-19 17:18:49 +0700

    push Test For Failure

--- romantest.py
+++ romantest.py
@@ -68,6 +68,34 @@ class KnownValues(unittest.TestCase): # (1)
         for integer, numeral in self.knownValues:
             result = roman.fromRoman(numeral) # (4) (5)
             self.assertEqual(integer, result) # (6)
+class ToRomanBadInput(unittest.TestCase):
+    def testTooLarge(self):
+        """toRoman should fail with large input"""
+        self.assertRaises(roman.OutOfRangeError, roman.toRoman, 4000)
+    def testZero(self):
+        """toRoman should fail with 0 input"""
+        self.assertRaises(roman.OutOfRangeError, roman.toRoman, 0)
+    def testNegative(self):
+        """toRoman should fail with negative input"""
+        self.assertRaises(roman.OutOfRangeError, roman.toRoman, -1)
+    def testNonInteger(self):
+        """toRoman should fail with non-integer input"""
+        self.assertRaises(roman.NotIntegerError, roman.toRoman, 0.5)
+
+class FromRomanBadInput(unittest.TestCase):
+    def testTooManyRepeatedNumerals(self):
+"""fromRoman should fail with too many repeated numerals"""
+        for s in ('MMMM', 'DD', 'CCCC', 'LL', 'XXXX', 'VV', 'IIII'):
+            self.assertRaises(roman.InvalidRomanNumeralError, roman.fromRoman, s)
+    def testRepeatedPairs(self):
+        """fromRoman should fail with repeated pairs of numerals"""
+        for s in ('CMCM', 'CDCD', 'XCXC', 'XLXL', 'IXIX', 'IVIV'):
+            self.assertRaises(roman.InvalidRomanNumeralError, roman.fromRoman, s)
+    def testMalformedAntecedent(self):
+        """fromRoman should fail with malformed antecedents"""
+        for s in ('IIMXCC', 'VX', 'DCM', 'CMM', 'IXIV','MCMC', 'XCX', 'IVI', 'LM', 'LD', 'LC'):
+            self.assertRaises(roman.InvalidRomanNumeralError, roman.fromRoman, s)
 # TODO: add more test cases here
 if __name__ == "__main__":
+    
 unittest.main()
