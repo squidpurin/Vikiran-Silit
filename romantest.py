431385a61928fb6fe2c07a2fc0a4db254c264291
squidpurin <punno519@gmail.com>
2018-04-19 17:22:44 +0700

    Fixed that shit nigga
    also added sanity shit

--- romantest.py
+++ romantest.py
@@ -68,6 +68,7 @@ class KnownValues(unittest.TestCase): # (1)
         for integer, numeral in self.knownValues:
             result = roman.fromRoman(numeral) # (4) (5)
             self.assertEqual(integer, result) # (6)
+
 class ToRomanBadInput(unittest.TestCase):
     def testTooLarge(self):
         """toRoman should fail with large input"""
@@ -84,7 +85,7 @@ class ToRomanBadInput(unittest.TestCase):
 
 class FromRomanBadInput(unittest.TestCase):
     def testTooManyRepeatedNumerals(self):
-"""fromRoman should fail with too many repeated numerals"""
+        """fromRoman should fail with too many repeated numerals"""
         for s in ('MMMM', 'DD', 'CCCC', 'LL', 'XXXX', 'VV', 'IIII'):
             self.assertRaises(roman.InvalidRomanNumeralError, roman.fromRoman, s)
     def testRepeatedPairs(self):
@@ -95,7 +96,28 @@ class FromRomanBadInput(unittest.TestCase):
         """fromRoman should fail with malformed antecedents"""
         for s in ('IIMXCC', 'VX', 'DCM', 'CMM', 'IXIV','MCMC', 'XCX', 'IVI', 'LM', 'LD', 'LC'):
             self.assertRaises(roman.InvalidRomanNumeralError, roman.fromRoman, s)
-# TODO: add more test cases here
+
+class SanityCheck(unittest.TestCase):
+    def testSanity(self):
+        """fromRoman(toRoman(n))==n for all n"""
+        for integer in range(1, 4000):
+            numeral = roman.toRoman(integer)
+            result = roman.fromRoman(numeral)
+            self.assertEqual(integer, result)
+
+class CaseCheck(unittest.TestCase):
+    def testToRomanCase(self):
+        """toRoman should always return uppercase"""
+        for integer in range(1, 4000):
+            numeral = roman.toRoman(integer)
+            self.assertEqual(numeral, numeral.upper()) # (1)
+        def testFromRomanCase(self):
+            """fromRoman should only accept uppercase input"""
+            for integer in range(1, 4000):
+                numeral = roman.toRoman(integer)
+                roman.fromRoman(numeral.upper()) # (2) (3)
+                self.assertRaises(roman.InvalidRomanNumeralError,
+                roman.fromRoman, numeral.lower())
+
 if __name__ == "__main__":
-    
-unittest.main()
+    unittest.main()
